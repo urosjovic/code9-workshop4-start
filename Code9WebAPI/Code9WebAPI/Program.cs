c13b39dad5ba7e98f4552ba6ce408954ad188367
Uros Jovic <uros.jovic@leaseplan.com>
2024-05-30 12:51:22 +0200

    initial commit

---
+++ Code9WebAPI/Code9WebAPI/Program.cs
@@ -0,0 +1,48 @@
+using Code9.Domain.Commands;
+using Code9.Domain.Interfaces;
+using Code9.Infrastructure;
+using Code9.Infrastructure.Repositories;
+using Code9WebAPI.Validators;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(builder.Configuration));
+
+// Add services to the container.
+builder.Services.AddDbContext<CinemaDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CinemaConnStr")));
+
+builder.Services.AddScoped<ICinemaRepository, CinemaRepository>();
+builder.Services.AddScoped<IMovieRepository, MovieRepository>();
+
+builder.Services.AddMediatR(config => config.RegisterServicesFromAssembly(typeof(AddCinemaCommand).Assembly));
+
+builder.Services.AddControllers();
+
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<AddCinemaValidator>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,1 @@
+# code9-workshop4-start
