a7313abf610274e735591153e423e28866437d06
Uros Jovic <uros.jovic@leaseplan.com>
2024-06-03 08:37:48 +0200

    feat(api): add azure integration

--- Code9WebAPI/Code9WebAPI/Program.cs
+++ Code9WebAPI/Code9WebAPI/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Monitor.OpenTelemetry.AspNetCore;
 using Code9.Domain.Commands;
 using Code9.Domain.Interfaces;
 using Code9.Infrastructure;
@@ -29,6 +30,12 @@ builder.Services.AddValidatorsFromAssemblyContaining<AddCinemaValidator>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+if(!builder.Environment.IsDevelopment())
+{
+    builder.Services.AddOpenTelemetry().UseAzureMonitor();
+}
+
 
 var app = builder.Build();
 
